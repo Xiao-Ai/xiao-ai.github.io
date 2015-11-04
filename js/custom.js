a677147eeb4f776350130ec2a4fb52a9027dd062
Xiao Ai <xiao.ai@maxwit.com>
2015-11-04 21:54:14 +0800

    modified index.html. Add scroll to top.
    Signed-off-by: Xiao Ai <xiao.ai@maxwit.com>

--- js/custom.js
+++ js/custom.js
@@ -1,7 +1,18 @@
-$(document).ready(function(){
-    // $("li").hover(function(){
-    //     $("this").hide();
-    // }, function() {
-    // 	$("this").show();
-    // });
+$(document).ready(function() {
+    $(function() {
+        $(window).scroll(function() {
+            if ($(window).scrollTop() > 100) {
+                $("#back-to-top").fadeIn(1500);
+            } else {
+                $("#back-to-top").fadeOut(1500);
+            }
+        });
+
+        $("#back-to-top").click(function() {
+            $('body,html').animate({
+                scrollTop: 0
+            }, 500);
+            return false;
+        });
+    });
 });
